786c78b8de5fdea79c1caf2c692ef64792969db3
SakamakiRyu <potesara031w@gmail.com>
2022-09-25 14:55:09 +0900

    Sceneの追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,34 @@
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// ゲームの進行を管理するクラス
 /// </summary>
 public class GameManager : MonoBehaviour
 {
+    public enum Scene
+    {
+        None = -1,
+        Title,
+        InGame
+    }
+
+    public Scene CurrentScene { get; private set; } = Scene.None;
+
+    #region Unity Function
     private void Start()
     {
-        
+        ChengeScene(Scene.Title);
+    }
+    #endregion
+
+    /// <summary>
+    /// シーンの変更
+    /// </summary>
+    private void ChengeScene(Scene nextScene)
+    {
+        CurrentScene = nextScene;
+        SceneManager.LoadScene((int)nextScene);
     }
 }
